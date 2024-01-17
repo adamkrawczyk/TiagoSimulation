803aad986fa168503f04611c9a6ee0594623eecf
Adam Krawczyk <adam.krawczyk@robotec.ai>
2024-01-17 16:06:04 +0100

    unify launches
    Signed-off-by: Adam Krawczyk <adam.krawczyk@robotec.ai>

--- launches/slam_navigation/launch/laser_filter.launch.py
+++ launches/slam_navigation/launch/laser_filter.launch.py
@@ -7,12 +7,13 @@ from launch_ros.actions import Node
 from ament_index_python.packages import get_package_share_directory
 
 def generate_launch_description():
+    print(str(pathlib.Path(__file__).parent.absolute().joinpath('config', 'laser_filter.yaml')))
     return LaunchDescription([
         Node(
             package="laser_filters",
             executable="scan_to_scan_filter_chain",
             arguments=[
-                '-d', str(pathlib.Path(__file__).parent.absolute().joinpath('config', 'footprint_filter_example.yaml'))
+                '-d', str(pathlib.Path(__file__).parent.absolute().joinpath('config', 'laser_filter.yaml'))
             ],
             # remappings=[("scan", "base_footprint/scan")]
         )
